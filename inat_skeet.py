744c8d54e94baba9c4b6ed9cdfb175775ea8406f
rjmendez <rjmendez@trunkfullofdeadhookers.com>
2025-02-24 12:31:55 -0500

    Allowing multiple observations per page.

--- inat_skeet.py
+++ inat_skeet.py
@@ -13,7 +13,7 @@ user_id_num = os.getenv('INAT_ID_NUMBER')
 my_handle = os.getenv('BSKY_HANDLE')
 my_password = os.getenv('BSKY_PASSWORD')
 
-prompt = "Please provide a thoroughly detailed description of this image. This image probably contains the organism "
+prompt = "Please provide an extemely brief visual description of this image. This image contains an organism named "
 
 # Get number of observations for pagination
 num_obs = get_user_by_id(user_id_num)
@@ -51,43 +51,34 @@ def post_images(fullname, observed_on, place_guess, uri, paths, iconic_taxon_nam
     client.login(my_handle, my_password)
     # This is horrible, I need to unfuck this completely
     if iconic_taxon_name == 'Insecta':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('insects','insects').text('\n').tag('inverts','inverts').text('\n').tag('bugs','bugs').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('insects','insects').text('\n').tag('inverts','inverts').text('\n').tag('bugs','bugs').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Animalia':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('animals','animals').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('animals','animals').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Aves':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('birds','birds').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('birds','birds').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Amphibia':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('amphibians','amphibians').text('\n').tag('herps','herps').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('amphibians','amphibians').text('\n').tag('herps','herps').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Reptilia':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('reptiles','reptiles').text('\n').tag('herps','herps').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('reptiles','reptiles').text('\n').tag('herps','herps').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Mammalia':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('mammals','mammals').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('mammals','mammals').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Actinopterygii':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('fish','fish').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('fish','fish').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Mollusca':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('molluscs','molluscs').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('molluscs','molluscs').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Arachnida':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('arachnids','arachnids').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('arachnids','arachnids').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Plantae':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('plants','plants').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('plants','plants').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Fungi':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('fungi','fungi').text('\n').tag('mycology','mycology').text('\n').tag('mushrooms','mushrooms').text('\n').tag('fungifriends','fungifriends').text('\nWarning! AI generated alt text.'), images=images,  image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('fungi','fungi').text('\n').tag('mycology','mycology').text('\n').tag('mushrooms','mushrooms').text('\n').tag('fungifriends','fungifriends').text('\nWarning! AI generated alt text.')
     elif iconic_taxon_name == 'Protozoa':
-        client.send_images(
-        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('Protozoa','Protozoa').text('\nWarning! AI generated alt text.'), images=images, image_alts=alt_text,)
+        text=client_utils.TextBuilder().text(fullname).text('\n\n').text('Quality grade: '+quality_grade+'\n').text(observed_on).text('\n').text(place_guess).text('\n').link(uri, uri).text('\n\n').tag('macrophotography','macrophotography').text('\n').tag('nature','nature').text('\n').tag('inaturalist','inaturalist').text('\n').tag(iconic_taxon_name, iconic_taxon_name).text('\n').tag('Protozoa','Protozoa').text('\nWarning! AI generated alt text.')
     else:
         pass
+    client.send_images(
+        text=text, images=images, image_alts=alt_text,)
+    
     # 🤮🤮🤮🤮🤮🤮🤮🤮
 
 def getobs(user_id, per_page):
@@ -95,52 +86,55 @@ def getobs(user_id, per_page):
         observations = get_observations(user_id=user_id, page=page, per_page=per_page)
         my_observations = Observation.from_json_list(observations)
         # Lets pull single observations by ID
-        inat_ids = my_observations[0].id
+        inat_ids = []
+        for inat_id in my_observations:
+            inat_ids.append(inat_id.id)
         return(inat_ids)
 
 def obs_details():
     obs = getobs(user_id, per_page)
-    observation = get_observations_by_id(obs)['results'][0]
-    photo_urls = []
-    for photo in observation['photos']:
-        url = photo['url']
-        photo_urls.append(url.replace("square", "original"))
+    for id in obs:
+        observation = get_observations_by_id(id)['results'][0]
+        photo_urls = []
+        for photo in observation['photos']:
+            url = photo['url']
+            photo_urls.append(url.replace("square", "original"))
     
-    # Extract details
-    try:
-        taxon_name = str(observation['taxon']['name'])
-    except:
-        taxon_name = ' '
-    try:
-        preferred_common_name = str(observation['taxon']['preferred_common_name'])
-    except:
-        preferred_common_name = ' '
-    try:
-        iconic_taxon_name = str(observation['taxon']['iconic_taxon_name'])
-    except:
-        iconic_taxon_name = ' '
-    try:
-        quality_grade = observation['quality_grade']
-    except:
-        quality_grade = ' '
-    fullname = taxon_name+' - '+preferred_common_name
-    uri = observation['uri']
-    observed_on = str(observation['observed_on'])
-    place_guess = observation['place_guess']
-    inat_id = observation['id']
-    images = download_images(inat_id, photo_urls)
-    # Done extracting
-
-    post_images(fullname, observed_on, place_guess, uri, images, iconic_taxon_name, quality_grade)
+        # Extract details
+        try:
+            taxon_name = str(observation['taxon']['name'])
+        except:
+            taxon_name = ' '
+        try:
+            preferred_common_name = str(observation['taxon']['preferred_common_name'])
+        except:
+            preferred_common_name = ' '
+        try:
+            iconic_taxon_name = str(observation['taxon']['iconic_taxon_name'])
+        except:
+            iconic_taxon_name = ' '
+        try:
+            quality_grade = observation['quality_grade']
+        except:
+            quality_grade = ' '
+        fullname = taxon_name+' - '+preferred_common_name
+        uri = observation['uri']
+        observed_on = str(observation['observed_on'])
+        place_guess = observation['place_guess']
+        inat_id = observation['id']
+        images = download_images(inat_id, photo_urls)
+        # Done extracting
+    
+        post_images(fullname, observed_on, place_guess, uri, images, iconic_taxon_name, quality_grade)
 
 def download_images(id, photo_urls):
     img_index = 0
     photo_names = []
     observation = id
     for img_url in photo_urls:
-        img_index = img_index+1
         data = requests.get(img_url).content
         filename = str(id)+str('_'+str(img_index))+'.jpg'
+        img_index = img_index+1
         f = open(filename,'wb')
         f.write(data)
         f.close()
@@ -162,7 +156,7 @@ def recompress_image(filepath):
     picture.save(new_filename,  
                  optimize = True, 
                  subsampling=0, 
-                 quality = 75) 
+                 quality = 70) 
     return(new_filename)
 
 
